e416337bfae74ee3872d79f8bb58afcd55611a78
jung <ky2059@naver.com>
2025-05-14 17:32:45 +0900

    server_client_cert

--- create_cert.sh
+++ create_cert.sh
@@ -3,20 +3,57 @@
 # version show
 openssl version
 
+# CA Cert
+echo -e "\033[34m CA Certificate Make\033[0m"
+
 # create encryption CA private key
-openssl genrsa -aes128 -out ca.key 2048
+openssl genrsa -aes128 -out CA/ca.key 2048
 
-# create CSR(certificate sign request)
+# create CA CSR(certificate signing request)
 # A challenge password: option label text filed, if not use -> just enter
-openssl req -new -key ca.key -out ca.csr
+openssl req -new -key CA/ca.key -out CA/ca.csr
 
-# create self signed CA certificate by reference CSR
-openssl x509 -req -in ca.csr -signkey ca.key -out ca.crt -days 365 -sha256
+# create self signed CA certificate by reference CA CSR
+openssl x509 -req -in CA/ca.csr -signkey CA/ca.key -out CA/ca.crt -days 365 -sha256
 
 echo -e "\033[32m created CA certificate\033[0m"
 
 # show crt file
-openssl x509 -in ca.crt -text -noout
+openssl x509 -in CA/ca.crt -text -noout
+
+# Server Cert
+echo -e "\033[34m Server Certificate Make\033[0m"
+
+# create encryption server private key
+openssl genrsa -aes128 -out server/server.key 2048
+
+# create server CSR(certificate signing request)
+openssl req -new -key server/server.key -out server/server.csr
+
+# create CA signed server certificate by reference server CSR
+openssl x509 -req -in CA/ca.csr -CA CA/ca.crt -CAkey CA/ca.key \
+    -CAcreateserial -out server/server.crt -days 365 -sha256
+
+echo -e "\033[32m created Server certificate\033[0m"
+
+# Client Cert
+echo -e "\033[34m Client Certificate Make\033[0m"
+
+# create encryption client private key
+openssl genrsa -aes128 -out client/client.key 2048
+
+# create client CSR(certificate signing request)
+openssl req -new -key client/client.key -out client/client.csr
+
+# create CA signed client certificate by reference server CSR
+openssl x509 -req -in CA/ca.csr -CA CA/ca.crt -CAkey CA/ca.key \
+    -CAcreateserial -out client/client.crt -days 365 -sha256
+
+echo -e "\033[32m created Client certificate\033[0m"
+
+
+
+
 
 
 
