b9db8a2c32fc0182fa9d434a4fa57d7f88b0040f
jung <ky2059@naver.com>
2025-05-29 16:55:17 +0900

    CA_sign_cert

--- create_cert.sh
+++ create_cert.sh
@@ -7,7 +7,7 @@ openssl version
 echo -e "\033[34m CA Certificate Make\033[0m"
 
 # create encryption CA private key
-openssl genrsa -aes128 -out CA/ca.key 2048
+openssl genrsa -out CA/ca.key 2048
 
 # create CA CSR(certificate signing request)
 # A challenge password: option label text filed, if not use -> just enter
@@ -25,13 +25,13 @@ openssl x509 -in CA/ca.crt -text -noout
 echo -e "\033[34m Server Certificate Make\033[0m"
 
 # create encryption server private key
-openssl genrsa -aes128 -out server/server.key 2048
+openssl genrsa -out server/server.key 2048
 
 # create server CSR(certificate signing request)
 openssl req -new -key server/server.key -out server/server.csr
 
 # create CA signed server certificate by reference server CSR
-openssl x509 -req -days 365 -in server/server.csr -signkey server/server.key -out server/server.crt
+openssl x509 -req -days 365 -in server/server.csr -CA CA/ca.crt -CAkey CA/ca.key -CAcreateserial -out server/server.crt -sha256
 
 echo -e "\033[32m created Server certificate\033[0m"
 
@@ -39,13 +39,13 @@ echo -e "\033[32m created Server certificate\033[0m"
 echo -e "\033[34m Client Certificate Make\033[0m"
 
 # create encryption client private key
-openssl genrsa -aes128 -out client/client.key 2048
+openssl genrsa -out client/client.key 2048
 
 # create client CSR(certificate signing request)
 openssl req -new -key client/client.key -out client/client.csr
 
 # create CA signed client certificate by reference server CSR
-openssl x509 -req -days 365 -in client/client.csr -signkey client/client.key -out client/client.crt
+openssl x509 -req -days 365 -in client/client.csr -CA CA/ca.crt -CAkey CA/ca.key -CAcreateserial -out client/client.crt -sha256
 
 echo -e "\033[32m created Client certificate\033[0m"
 
